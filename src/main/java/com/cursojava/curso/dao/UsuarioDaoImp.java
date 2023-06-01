52a0f5f48aefbf55141e0c8fb80bad3b71f2e169
Anías Borja <joseaniasb@gmail.com>
2023-06-01 13:40:15 -0600

    commit antes de agregar dependencia lombok

--- src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
@@ -2,18 +2,29 @@ package com.cursojava.curso.dao;
 
 import java.util.List;
 
+import javax.swing.text.html.parser.Entity;
+
 import org.springframework.stereotype.Repository;
 
 import com.cursojava.curso.models.Usuario;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @Repository
 @Transactional
 public class UsuarioDaoImp implements UsuarioDao {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<Usuario> getUsuarios() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getUsuarios'");
+        
+        String query ="FROM Usuario";
+        return entityManager.createQuery(query).getResultList();
+        
+        
     }
     
 }
--- src/main/java/com/cursojava/curso/models/Usuario.java
+++ src/main/java/com/cursojava/curso/models/Usuario.java
@@ -1,5 +1,10 @@
 package com.cursojava.curso.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
     private Long id;
