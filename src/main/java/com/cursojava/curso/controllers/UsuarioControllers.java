043f63f0901390db990ab766067ff136d1ffa45b
Anías Borja <joseaniasb@gmail.com>
2023-06-08 20:15:08 -0600

    commit con conexión a db mysql debido a inconvenientes instalación de oracle sql

--- src/main/java/com/cursojava/curso/controllers/UsuarioControllers.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioControllers.java
@@ -1,16 +1,20 @@
 package com.cursojava.curso.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cursojava.curso.dao.UsuarioDao;
 import com.cursojava.curso.models.Usuario;
 
 @RestController
 public class UsuarioControllers {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
 
     @RequestMapping (value = "usuario/{id}")
     public Usuario getUsuario(@PathVariable Long id){
@@ -25,40 +29,7 @@ public class UsuarioControllers {
 
     @RequestMapping (value = "usuarios")
     public List<Usuario> getUsuarios (Long id){
-        List <Usuario> usuarios = new ArrayList<>();
-        Usuario usuario = new Usuario();
-        usuario.setId(111L);
-        usuario.setNombre("Anias");
-        usuario.setApellido("Borja");
-        usuario.setEmail("a321borja@mail.com");
-        usuario.setTelefono("45455444");
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(222L);
-        usuario2.setNombre("Anias");
-        usuario2.setApellido("Borja");
-        usuario2.setEmail("a321borja@mail.com");
-        usuario2.setTelefono("45455444");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(333L);
-        usuario3.setNombre("Anias");
-        usuario3.setApellido("Borja");
-        usuario3.setEmail("a321borja@mail.com");
-        usuario3.setTelefono("45455444");
-
-        Usuario usuario4 = new Usuario();
-        usuario4.setId(444L);
-        usuario4.setNombre("Anias");
-        usuario4.setApellido("Borja");
-        usuario4.setEmail("a321borja@mail.com");
-        usuario4.setTelefono("45455444");
-
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-        usuarios.add(usuario4);
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
     
     @RequestMapping (value = "usuario1")
--- src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
@@ -1,15 +1,14 @@
 package com.cursojava.curso.dao;
 
-import java.util.List;
-
-import javax.swing.text.html.parser.Entity;
-
-import org.springframework.stereotype.Repository;
-
 import com.cursojava.curso.models.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+
 
 @Repository
 @Transactional
@@ -19,11 +18,12 @@ public class UsuarioDaoImp implements UsuarioDao {
     private EntityManager entityManager;
 
     @Override
+    
     public List<Usuario> getUsuarios() {
         
-        String query ="FROM Usuario";
-        return entityManager.createQuery(query).getResultList();
+        String consulta = "FROM Usuario";
         
+        return entityManager.createQuery(consulta,Usuario.class).getResultList();
         
     }
     
